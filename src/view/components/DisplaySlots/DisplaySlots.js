df054ec844e3f7e39a07a9cd775e557ca1009e69
George Novitskiy <novitskiy.gs@gmail.com>
2022-03-13 16:56:56 +0300

    add body machine

--- src/view/components/DisplaySlots/DisplaySlots.js
+++ src/view/components/DisplaySlots/DisplaySlots.js
@@ -6,6 +6,9 @@ import {
   setStatusBtnStartOn,
   setStatusBtnStopOff,
 } from "../redux/actions/toggleStatusButton.js";
+import { MachineBody } from "../MachineBody";
+import { Physics } from "@react-three/cannon";
+import { Frame } from "../Frame/Frame";
 
 export const DisplaySlots = () => {
   const statusGame = useSelector((state) => state.statusGame);
@@ -19,26 +22,29 @@ export const DisplaySlots = () => {
   return (
     <Suspense fallback={null}>
       <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={0.1} />
-        <pointLight position={[10, 10, 10]} />
-        <SlotDrums
-          changeStatusButton={() => changeStatusButton()}
-          statusGame={statusGame}
-          position={[-3, 0, 0]}
-          rotation={[Math.PI, 0, Math.PI / 2]}
-        ></SlotDrums>
-        <SlotDrums
-          changeStatusButton={() => changeStatusButton()}
-          statusGame={statusGame}
-          position={[0, 0, 0]}
-          rotation={[Math.PI, 0, Math.PI / 2]}
-        ></SlotDrums>
-        <SlotDrums
-          changeStatusButton={() => changeStatusButton()}
-          statusGame={statusGame}
-          position={[3, 0, 0]}
-          rotation={[Math.PI, 0, Math.PI / 2]}
-        ></SlotDrums>
+        <Physics>
+          <ambientLight intensity={0.1} />
+          <pointLight position={[10, 10, 10]} />
+          <SlotDrums
+            changeStatusButton={() => changeStatusButton()}
+            statusGame={statusGame}
+            position={[-2.3, 0, 0]}
+            rotation={[Math.PI, 0, Math.PI / 2]}
+          ></SlotDrums>
+          <SlotDrums
+            changeStatusButton={() => changeStatusButton()}
+            statusGame={statusGame}
+            position={[0, 0, 0]}
+            rotation={[Math.PI, 0, Math.PI / 2]}
+          ></SlotDrums>
+          <SlotDrums
+            changeStatusButton={() => changeStatusButton()}
+            statusGame={statusGame}
+            position={[2.3, 0, 0]}
+            rotation={[Math.PI, 0, Math.PI / 2]}
+          ></SlotDrums>
+          <MachineBody />
+        </Physics>
       </Canvas>
     </Suspense>
   );
---
+++ src/view/components/MachineBody/MachineBody.js
@@ -0,0 +1,19 @@
+import { usePlane } from "@react-three/cannon";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
+export const MachineBody = () => {
+  const body = useLoader(TextureLoader, "./img/body2.png");
+
+  const [ref] = usePlane(() => ({
+    rotation: [0, 0, 0],
+    position: [0, 0.12, 0.4],
+  }));
+
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[11, 4]} />
+      <meshPhysicalMaterial map={body} />
+    </mesh>
+  );
+};
